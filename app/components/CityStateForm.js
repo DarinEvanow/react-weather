aad7c731ae52c0844f93b4398f4f6fc63ec69c39
Darin <darinevanow@gmail.com>
2018-03-27 15:26:24 -0700

    Added a header component that reuses the CityStateForm, utilizing a prop to switch the direction based on whether it is in the navbar, or in the center of the page.

--- app/components/CityStateForm.js
+++ app/components/CityStateForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function CityStateForm () {
+export default function CityStateForm (props) {
   return (
     <div>
-      <form class='city-state-form'>
+      <form class='city-state-form' style={{flexDirection: props.direction}}>
         <input class='form-control' type='text' name='citystate' placeholder='San Francisco, California' />
         <input class='btn btn-success' type='submit' value='Get Weather' />
       </form>
