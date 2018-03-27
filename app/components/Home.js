bdf09f51a7ca6a23a55b8012347e4389f756b045
Darin <darinevanow@gmail.com>
2018-03-26 19:32:56 -0700

    Created basic homepage for application, by adding in a header, top nav, background image, and basic CSS.

---
+++ app/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home () {
+  return (
+    <div className='home>'>
+      <h1 className='header'>Enter a City and State</h1>
+    </div>
+  )
+}
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom';
+import Nav from '../components/Nav';
+import Home from '../components/Home';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <div className='container'>
+        <div>
+          <Nav />
+        </div>
+        <div className='home-container' style={{backgroundImage: "url('app/images/pattern.svg')"}}>
+          <Home />
+        </div>
+      </div>
     )
   }
 }
