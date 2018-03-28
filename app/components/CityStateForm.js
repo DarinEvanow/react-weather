b2fd28280b60cb40852dea5c10add3dfb18b1d02
Darin <darinevanow@gmail.com>
2018-03-28 12:57:26 -0700

    Converted CityStateForm to a class, and created basic functions to handle form input change, and form submit.

--- app/components/CityStateForm.js
+++ app/components/CityStateForm.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function CityStateForm (props) {
-  return (
-    <div>
-      <form class='city-state-form' style={{flexDirection: props.direction}}>
-        <input class='form-control' type='text' name='citystate' placeholder='San Francisco, California' />
-        <input class='btn btn-success' type='submit' value='Get Weather' />
-      </form>
-    </div>
-  )
+class CityStateForm extends React.Component {
+  static propTypes = {
+    cityAndState: PropTypes.string.isRequired,
+  }
+
+  state = {
+    cityAndState: '',
+  }
+
+  handleUpdateCityState = (event) => {
+    const value = event.target.value;
+
+    this.setState(() => ({cityAndState: value}));
+  }
+
+  handleSubmitCityState = (event) => {
+    event.preventDefault();
+
+    console.log(this.state.cityAndState);
+  }
+
+  render () {
+    return (
+      <div>
+        <form className='city-state-form'
+              style={{flexDirection: this.props.direction}}
+              onSubmit={this.handleSubmitCityState}>
+          <input className='form-control'
+                 type='text' name='citystate'
+                 placeholder='San Francisco, California'
+                 onChange={this.handleUpdateCityState} />
+          <input className='btn btn-success'
+                 type='submit'
+                 value='Get Weather' />
+        </form>
+      </div>
+    )
+  }
 }
+
+export default CityStateForm;
